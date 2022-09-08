c6278e01b0320f35fe192dc56c8736eabd896d0d
Karl <karl.lu@outlook.com>
2022-09-08 00:02:39 -0400

    created todolist and added minorstyling

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,25 @@
-export default function TodoList(){
-    return(
-        <h1>hello</h1>
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+export default function TodoList(props) {
+    return (
+        <Paper>
+            <List>
+                {props.todos.map(todo => (
+                    // the <> & </> are JSX fragments to allow adjacent JSX elements
+                    <>
+                        <ListItem>
+                            <ListItemText>
+                                {todo.task}
+                            </ListItemText>
+                        </ListItem>
+                        <Divider />
+                    </>
+                ))}
+            </List>
+        </Paper>
     )
 }
